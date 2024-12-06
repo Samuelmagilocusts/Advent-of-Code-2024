49d175cadb317d47dbde21b623d6c61be79fbf7d
Samuelmagilocusts <64rcmsm@gmail.com>
2024-12-05 19:33:39 -0500

    Day 5 Complete P1,P2

--- Day 5/main.js
+++ Day 5/main.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
-const file_op = fs.readFileSync("C:/Users/Matthew/OneDrive - Schoolhouse Electronics LLC/Desktop/Extra/Coding projects/AOC 2024/Advent-of-Code-2024/Day 5/input_operations.txt", "utf8");
-const file_pages = fs.readFileSync("C:/Users/Matthew/OneDrive - Schoolhouse Electronics LLC/Desktop/Extra/Coding projects/AOC 2024/Advent-of-Code-2024/Day 5/input_pages.txt", "utf8");
-// console.log(file_pages);
+const file_op = fs.readFileSync("input_operations.txt", "utf8");
+const file_pages = fs.readFileSync("input_pages.txt", "utf8");
+// const file_op = fs.readFileSync("test1.txt", "utf8");
+// const file_pages = fs.readFileSync("test2.txt", "utf8");
 
 const array_op_string = file_op.split('\r\n');
 const array_pages_string = file_pages.split('\r\n');
@@ -11,12 +12,10 @@ var pages_2darray = [];
 array_op_string.forEach(line => {
     let pair = [];
     let [left, right] = line.split('|');
-    // console.log("left",left,"right",right)
     pair.push(parseInt(left));
     pair.push(parseInt(right));
     operations.push(pair);
 });
-// console.log(operations);
 
 array_pages_string.forEach(line => {
     let int_pages = [];
@@ -30,46 +29,77 @@ array_pages_string.forEach(line => {
 function get_constraints(page) {
     let commands = [];
     operations.forEach(operation_pair => {
-        // console.log("31",operation_pair,"page",page);
         if (page.includes(operation_pair[0]) && page.includes(operation_pair[1])) {
             commands.push(operation_pair);
-        }
+        } 
     });
     return commands;
 }
+
 function validate_page(commands, page) {
+    let answer = true;
     commands.forEach(command => {
-        // console.log(command)
-        if (command[0] < command[1]) {
-            if (page.indexOf(command[0]) > page.indexOf(command[1])) {
-                return false;
-            }
-        } else {
-            if (page.indexOf(command[0]) < page.indexOf(command[1])) {
-                return false;
-            }
+        let left = page.indexOf(command[0]);
+        let right = page.indexOf(command[1]);
+        if (left >= right) {
+            answer = false;
         }
     });
-    return true;
+    return answer;
+}
+
+function fix_page(page, commands) {
+    let fixed_page = structuredClone(page); 
+    let fixing = true;
+    while (fixing) {
+        fixing = false;
+        commands.forEach(command => {
+            let left_index = fixed_page.indexOf(command[0]);
+            let right_index = fixed_page.indexOf(command[1]);
+            if (left_index > right_index) {
+                fixing = true;
+                const [pull] = fixed_page.splice(left_index, 1);
+                if (right_index > 0) {
+                    fixed_page.splice(right_index-1,0,pull);
+                } else {
+                    fixed_page.unshift(pull);
+                }
+                let review;
+            }
+        })
+    }
+    return fixed_page;
 }
 
 
 function main() {
     var validated_pages = [];
+    var incorrectly_ordered = [];
     var total = 0;
+    var total_2 = 0;
 
     pages_2darray.forEach(page => {
         let commands = get_constraints(page);
-        if (validate_page(commands, page)) {
+        let valid = validate_page(commands, page);
+        if (valid) {
             validated_pages.push(page);
+        } else {
+            incorrectly_ordered.push(fix_page(page, commands));
         }
     })
 
     validated_pages.forEach(page => {
-        total += page[Math.round(page.length/2)];
+        let index = Math.round(page.length/2)-1;
+        total += page[index];
+    })
+
+    incorrectly_ordered.forEach(page => {
+        let index = Math.round(page.length/2)-1;
+        total_2 += page[index];
     })
 
     console.log("AOC Day5 Part 1 Total:",total);
+    console.log("AOC Day 5 Part 2 Totla:",total_2);
 }
 
 main();
