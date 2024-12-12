bd45f403519428ec0249ce8b5d8354e2226f7402
Schoolhouse-Electronics-Admin <matt@schoolhouseelectronics.com>
2024-12-12 13:16:18 -0500

    Started Day 12

---
+++ Day 12/aoc_day_12/src/main.rs
@@ -0,0 +1,75 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::collections::{HashMap, HashSet};
+use std::time::Instant;
+
+fn main() -> io::Result<()> {
+    let path = "test2.txt";
+    let alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    let file = File::open(path)?;
+    let reader = io::BufReader::new(file);
+    let mut total: i32 = 0;
+    let mut lines: Vec<Vec<char>> = reader
+        .lines()
+        .filter_map(Result::ok)
+        .map(|line| line.chars().collect())
+        .collect();
+    let mut area: HashMap<char, i32> = HashMap::new();
+    let mut per: HashMap<char, i32> = HashMap::new();
+
+    for (y, line) in lines.iter().enumerate() {
+        for (x, character) in line.iter().enumerate() {
+            area.entry(*character)
+                        .and_modify(|num_of_area| *num_of_area += 1)
+                        .or_insert(1);
+            
+            if x > 0 { // l
+                if lines[y][x-1] != *character {
+                    per.entry(*character)
+                        .and_modify(|num_of_per| *num_of_per += 1)
+                        .or_insert(1);
+                }
+            } 
+
+            if y > 0 { // u
+                if lines[y-1][x] != *character {
+                    per.entry(*character)
+                        .and_modify(|num_of_per| *num_of_per += 1)
+                        .or_insert(1);
+                }
+            } 
+
+            if x < line.len()-1 { // r
+                // println!("{},{}",lines[y][x+1],character);
+                if lines[y][x+1] != *character {
+                    per.entry(*character)
+                        .and_modify(|num_of_per| *num_of_per += 1)
+                        .or_insert(1);
+                }
+            }
+
+            if y < line.len()-1 { // d
+                if lines[y+1][x] != *character {
+                    per.entry(*character)
+                        .and_modify(|num_of_per| *num_of_per += 1)
+                        .or_insert(1);
+                }
+            }
+        }
+    }
+
+    println!("{:?}",area);
+    println!("{:?}",per);
+
+    for c in alphabet {
+        if let Some(a) = area.get(&c) {
+            if let Some(p) = per.get(&c) {
+                total += *a * *p;
+            }
+        }
+    }
+
+    println!("AOC Day 12 P1 Total: {}",total);
+
+    Ok(())
+}
---
+++ Day 12/aoc_day_12/target/debug/deps/aoc_day_12-8246ff2730b1c420.d
@@ -0,0 +1,5 @@
+C:\Users\Matthew\OneDrive - Schoolhouse Electronics LLC\Desktop\Extra\Coding projects\AOC 2024\Advent-of-Code-2024\Day 12\aoc_day_12\target\debug\deps\libaoc_day_12-8246ff2730b1c420.rmeta: src/main.rs
+
+C:\Users\Matthew\OneDrive - Schoolhouse Electronics LLC\Desktop\Extra\Coding projects\AOC 2024\Advent-of-Code-2024\Day 12\aoc_day_12\target\debug\deps\aoc_day_12-8246ff2730b1c420.d: src/main.rs
+
+src/main.rs:
