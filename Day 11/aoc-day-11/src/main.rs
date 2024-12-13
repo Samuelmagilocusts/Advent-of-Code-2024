3b370e42568037ab422f85eef0b88e13e0ddf219
Samuelmagilocusts <64rcmsm@gmail.com>
2024-12-13 01:15:00 -0500

    Day 11 Complete

--- Day 11/aoc-day-11/src/main.rs
+++ Day 11/aoc-day-11/src/main.rs
@@ -1,10 +1,7 @@
-use std::default;
 use std::io;
 use std::collections::HashSet;
 use std::collections::HashMap;
 use std::time::Instant;
-
-use rayon::collections::hash_map;
 
 fn remove_leading_zeros(s: &str) -> &str { 
     let trimmed = s.trim_start_matches('0'); 
@@ -60,65 +57,65 @@ fn is_even(number: u64) -> bool {
     }
 }
 
-// fn process(stones: Vec<u64>) {
-//     let mut amount_ones: i64 = 0;
-//     let mut amount_mul: i64 = 0;
-//     let mut aditional: i64 = 0;
-//     for stone in stones {
-//         if stone == 0 {
-//             amount_ones += 1;
-//         } else if is_even(stone) {
-//             aditional += 1;
-//         } else {
-//             stone * 2024;
-//         }
-//     }
-// }
 
-
-fn process(stones: Vec<u64>) -> Vec<u64> {
-    let mut new_vec: Vec<u64> = Vec::new();
-    for stone in stones {
+fn process(stones: &mut HashMap<u64, u64>, cashe: &mut HashMap<u64, [u64; 2]>) -> HashMap<u64, u64> {
+    let mut new_map: HashMap<u64, u64> = HashMap::new();
+    for (&stone, quantity) in stones {
         if stone == 0 {
-            new_vec.push(1);
+            new_map.entry(1).and_modify(|f|*f+=*quantity).or_insert(*quantity);
         } else if is_even(stone) == true {
-            let string_stone = stone.to_string();
-            let (left, right) = string_stone.split_at(string_stone.len() / 2);
-            let left_num = remove_leading_zeros(left).parse::<u64>().expect("Failed to parse left part");
-            let right_num = remove_leading_zeros(right).parse::<u64>().expect("Failed to parse right part");
-            new_vec.push(left_num); 
-            new_vec.push(right_num);
+            if let Some([left, right]) = cashe.get(&stone) {
+                new_map.entry(*left).and_modify(|f|*f+=*quantity).or_insert(*quantity);
+                new_map.entry(*right).and_modify(|f|*f+=*quantity).or_insert(*quantity);
+            } else {
+                let string_stone = stone.to_string();
+                let (left, right) = string_stone.split_at(string_stone.len() / 2);
+                let left_num = remove_leading_zeros(left).parse::<u64>().expect("Failed to parse left part");
+                let right_num = remove_leading_zeros(right).parse::<u64>().expect("Failed to parse right part");
+                new_map.entry(left_num).and_modify(|f|*f+=*quantity).or_insert(*quantity);
+                new_map.entry(right_num).and_modify(|f|*f+=*quantity).or_insert(*quantity);
+                cashe.entry(stone).insert_entry([left_num,right_num]);
+            }
         } else {
-            new_vec.push(stone * 2024);
+            let temp_mul: u64 = stone*2024;
+            new_map.entry(temp_mul).and_modify(|f|*f+=*quantity).or_insert(*quantity);
         }
     }
-    new_vec
+    new_map
 }
 
 fn main() -> io::Result<()> {
     let mut numbers: Vec<u64> = [112, 1110, 163902, 0, 7656027, 83039, 9, 74].to_vec();
     // let mut numbers: Vec<u64> = [125, 17].to_vec();
     let mut total: u64 = 0;
-    let mut workspace: Vec<u64> = Vec::new();
-    // let mut hash_map: HashMap<>
+    let mut hash_map: HashMap<u64, u64> = HashMap::new();
+    let mut cashe: HashMap<u64, [u64; 2]> = HashMap::new();
 
-    for (t, num) in numbers.iter().enumerate() {
-        workspace.push(*num);
-        let startMain = Instant::now(); 
-
-        for i in 0..75 {
-            let start = Instant::now(); 
-            workspace = process(workspace);
-            // println!("For {}, Time taken: {:?}, Size: {}",i,duration,numbers.len());
-            let duration = start.elapsed();
-            println!("Curent section: {}. Current blink: {}. Time elapsed: {:?}",t,i,duration);
-        }
-
-        total += workspace.len() as u64;
-        let durationMain = startMain.elapsed();
-        println!("Time taken: {:?} on section: {}",durationMain,t);
-        workspace.clear();
+    for num in numbers {
+        hash_map.insert(num, 1);
     }
+
+    let startMain = Instant::now(); 
+    for i in 0..75 {
+        let start = Instant::now(); 
+        hash_map = process(&mut hash_map,  &mut cashe);
+        
+        let duration = start.elapsed();
+        // println!("{:?}",hash_map);
+        // println!("Curent section: {}. Current blink: {}. Time elapsed: {:?}",t,i,duration);
+        println!("For {}, Time taken: {:?}",i,duration);
+    }
+
+    // println!("{:?}",hash_map);
+
+    for value in hash_map.values() {
+        total += value;
+    }
+    println!();
+
+    let durationMain = startMain.elapsed();
+    println!("Time taken: {:?}",durationMain);
+    
 
     println!("AOC Day 11 Part 2 Total: {}", total); 
 
